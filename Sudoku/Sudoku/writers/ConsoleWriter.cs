b6bd10b4f6eae2a2a828912334ca04bcf05fc1c8
Alon Shmilovich <alonshmilovich1302@gmail.com>
2022-01-15 17:28:27 +0200

    add console and file writers

---
+++ Sudoku/Sudoku/writers/ConsoleWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class ConsoleWriter : IWriter
+{
+
+
+    public ConsoleWriter()
+    {
+        // TODO get Formatter
+    }
+
+    public void Write(string data)
+    {
+        // TODO use formatter in print
+        Console.WriteLine(data);
+    }
+}
+
---
+++ Sudoku/Sudoku/writers/MainWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class MainWriter : IWriter
+{
+    private IList<IWriter> _writers;
+
+    public MainWriter()
+    {
+        this._writers = new List<IWriter>();
+    }
+
+    public void AddWriter(IWriter writer)
+    {
+        _writers.Add(writer);
+    }
+
+    public void Write(string data)
+    {
+        foreach (IWriter writer in _writers)
+        {
+            writer.Write(data);
+        }
+    }
+}
+
