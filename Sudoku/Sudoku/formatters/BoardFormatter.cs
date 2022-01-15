c39d2cfb46195d23b418c673f9671815c1e85c6a
Alon Shmilovich <alonshmilovich1302@gmail.com>
2022-01-15 17:26:42 +0200

    add board formatter

---
+++ Sudoku/Sudoku/formatters/BoardFormatter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class BoardFormatter : IFormatter
+{
+    public string Format(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Sudoku/Sudoku/formatters/IFormatter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+interface IFormatter
+{
+    public string Format(string input);
+}
+
