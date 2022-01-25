b54e06a75da0124b0026617101e50c77dcfb6a14
Alon Shmilovich <alonshmilovich1302@gmail.com>
2022-01-25 19:13:56 +0200

    few changes

--- Sudoku/Sudoku/formatters/BoardFormatter.cs
+++ Sudoku/Sudoku/formatters/BoardFormatter.cs
@@ -5,15 +5,26 @@ using System.Text;
 class BoardFormatter : IFormatter
 {
     const int CELL_SIZE = 3;
+    private int _rowSize;
+
     public string Format(string input)
     {
         SudokuBoard printBoard = new SudokuBoard(input);
+        _rowSize = printBoard.getSingleRowSize();
         string formattedBoard = "";
 
-        for (int i = 0; i < printBoard.getSingleRowSize(); i++)
+        for (int i = 0; i < _rowSize * CELL_SIZE; i++)
         {
-            for (int j = 0; j < printBoard.getSingleRowSize(); j++)
+            formattedBoard += '_';
+        }
+        formattedBoard += '\n';
+
+        for (int i = 0; i < _rowSize; i++)
+        {
+            formattedBoard += GetRowOfVerticalLines();
+            for (int j = 0; j < _rowSize; j++)
             {
+                formattedBoard += ' ';
                 if (printBoard[i, j] == '0')
                 {
                     formattedBoard += ' ';
